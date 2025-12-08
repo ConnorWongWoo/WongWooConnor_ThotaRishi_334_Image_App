b4e6c5b6b0b57e942fb25aa556ed0f7dcd3fba03
ConnorWongWoo <wongwoo3874@mydusd.org>
2025-12-07 20:08:39 -0800

    Implement image rotation and copy features

--- ImageApp.java
+++ ImageApp.java
@@ -61,9 +61,16 @@ public class ImageApp
     // Image #4 Using the original image and pixels, rotate it 180 degrees
     Picture upsidedownImage = new Picture(pictureFile);
     Pixel[][] upsideDownPixels = upsidedownImage.getPixels2D();
-    for (int i = 0; i < upsideDownPixels.length; i++) {
-      for (int j = 0; j < upsideDownPixels[i].length; j++) {
-        upsideDownPixels[i][j] = upsideDownPixels[j][i];
+    // use the original pixels as the source and upsideDownPixels as the destination
+    Pixel[][] src180 = origPixels; // reuse existing origPixels
+    int H = src180.length; // height (rows)
+    int W = src180[0].length; // width (cols)
+    // 180-degree rotation mapping: (x,y) -> (W-1-x, H-1-y)
+    for (int y = 0; y < H; y++) {
+      for (int x = 0; x < W; x++) {
+        int tx = (W - 1) - x;
+        int ty = (H - 1) - y;
+        upsideDownPixels[ty][tx].setColor(src180[y][x].getColor());
       }
     }
     upsidedownImage.explore();
@@ -72,17 +79,51 @@ public class ImageApp
     Pixel[][] rotatePixels = rotateImg.getPixels2D();
 
     /* to be implemented */
+    // rotate 90 degrees clockwise using matrix mapping
+    // source (x,y) -> target (y, W-1-x)
+    Pixel[][] src90 = rotateImg.getPixels2D();
+    Picture rot90Result = new Picture(origImg.getWidth(), origImg.getHeight());
+    Pixel[][] dst90 = rot90Result.getPixels2D();
+    int H90 = src90.length;
+    int W90 = src90[0].length;
+    for (int y = 0; y < H90; y++) {
+      for (int x = 0; x < W90; x++) {
+        int tx = y;
+        int ty = (W90 - 1) - x;
+        dst90[ty][tx].setColor(src90[y][x].getColor());
+      }
+    }
+    rot90Result.explore();
 
     // Image #6 Using the original image and pixels, rotate image -90
     Picture rotateImg2 = new Picture(pictureFile);
     Pixel[][] rotatePixels2 = rotateImg2.getPixels2D();
 
     /* to be implemented */
+    // rotate 90 degrees counter-clockwise using matrix mapping
+    // source (x,y) -> target (H-1-y, x)
+    Pixel[][] srcN90 = rotateImg2.getPixels2D();
+    Picture rotNeg90Result = new Picture(origImg.getWidth(), origImg.getHeight());
+    Pixel[][] dstN90 = rotNeg90Result.getPixels2D();
+    int Hn = srcN90.length;
+    int Wn = srcN90[0].length;
+    for (int y = 0; y < Hn; y++) {
+      for (int x = 0; x < Wn; x++) {
+        int tx = (Hn - 1) - y;
+        int ty = x;
+        dstN90[ty][tx].setColor(srcN90[y][x].getColor());
+      }
+    }
+    rotNeg90Result.explore();
 
 
     // Final Image: Add a small image to a larger one
 
-    /* to be implemented */
+    // Create a smaller version of the original and copy it into the big one
+    Picture big = new Picture(pictureFile);
+    Picture small = new Picture(pictureFile).getPictureWithWidth(Math.max(1, big.getWidth() / 4));
+    big.copy(small, 20, 20);
+    big.explore();
 
 
 
